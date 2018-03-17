7c983bbaf056ce8a40420577dca9fb927b2cbe49
Billy Levin <billylevs@gmail.com>
2018-03-17 12:06:12 +0000

    Fix api call

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
 
   getWeather = async (city, country) => {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
     );
     const data = await response.json();
     if (data.name) {
